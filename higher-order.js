983901e6a2b68b0f4a3f631a4d2fb42ec246469a
Andy Puertas <andy.puertas08@gmail.com>
2018-05-09 16:11:59 -0600

    done

--- higher-order.js
+++ higher-order.js
@@ -20,9 +20,15 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
+
+
+let evenNumbers = mixedNumbers.filter(num => num % 2 === 0)
+
+/* arr.filter(function(num){
+  num % 2 === 0;
+  return;
+}) */
 
 
 ////////// PROBLEM 2 //////////
@@ -44,7 +50,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map( price => {
+  return price * 1.07;
+});
 
 
 
@@ -63,7 +71,7 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation = populations.reduce((total, num) => total + num );
 
 
 
@@ -89,9 +97,12 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
-
+let myStrongest = monstersInYourPocket.filter((monster,i) => {
+  if(monster.CP > 200){
+    return monster;
+  }
+})
 
 ////////// PROBLEM 5 //////////
 
@@ -106,8 +117,10 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get the sum of all the order totals after adding in the sales tax
 */
 
-let ordersTotal //Code Here
-
+let ordersTotal = orders.map(order => {
+  order = order.price * (order.tax + 1);
+  return order;
+})
 
 
 ////////// PROBLEM 6 //////////
@@ -126,6 +139,6 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = purchases.filter(purchase => purchase.owner === 'Bob').map(order => order = order.price).reduce((total, price) => total + price)
 
 
