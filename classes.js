0845baa6c08739b2a0c0844eefcba66cddcd974a
Andy Puertas <andy.puertas08@gmail.com>
2018-09-17 23:58:30 -0600

    starting over

--- classes.js
+++ classes.js
@@ -2,12 +2,9 @@
   Once you complete a problem, refresh ./classes.html in your browser and check to see if the problem's test(s) are passing.
   Passed tests will be indicated by a green circle.
   Failed tests will be indicated by a red X.
-
   You can refresh the page at any time to re-run all the tests.
-
   Classes are a tool for building similar objects over and over again.
   They are a construct that helps your organize your code.
-
   Let's work with some employees at a company.
   You work for Widget Co. They have hundreds of employees.
 */
@@ -25,20 +22,18 @@
     - makeWidget
       - This returns a string equal to the employees first name + last name + the word widget
       - Example: "Dave Smith Widget"
-
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-class Employee {
-  
-}
+//Code Here
+
 
 
 ////////// PROBLEM 2 //////////
 
 /*
   Next, make a manager for Widget Co.
-  The manager has all the same properties as an Employee.
+  The manager has all the same properties as an Employee. Copy the Employee class and rename it Manager.
   Each manager has the following additional properties:
     - reports (other employees) that defaults to an empty array
   Each manager has the following additional methods:
@@ -46,7 +41,6 @@ class Employee {
       - Accepts a new employee as a parameter and pushes it to their list of reports.
     - fire (index)
       - Fire removes employees from their list of reports at the given index
-
   Call your new class Manager
 */
 
@@ -58,21 +52,18 @@ class Employee {
 
 /*
   Managers for Widget Co. get promoted when they get more employees, and get a bonus when they fire employees.
-  Progressive Managers have all the same properties as the manager,
-  but they also have the following additional properties:
+  Progressive managers have all the same properties as managers. Copy the Manager class and rename
+  it to ProgressiveManager. Add the following additional properties:
     - title - default 'Not a manager'
     - bonus - default 0
-
-  When employees are added or removed we need to check and update their title. Their titles are as follows:
-    0 : Not a manager
-    1-3 : Barely Manager
-    4-10 : Mostly Manager
-    11-50 : Manager
-    51-100 : Manager Plus
-    101+ : Bestest Manager
-
+  When employees are hired or fired, the manager's title should be updated based on the number of reports.
+    0 reports : Not a manager
+    1-3 reports : Barely Manager
+    4-10 reports : Mostly Manager
+    11-50 reports : Manager
+    51-100 reports : Manager Plus
+    101+ reports : Bestest Manager
   Everytime they fire an employee they get $100 added to their bonus.
-
   Call your new class ProgressiveManager
 */
 
@@ -85,13 +76,11 @@ class Employee {
 /*
   Widget Co has a factory that makes widgets.
   Factories have Machines.
-
   Make a Machine class that takes in no parameters
   A Machine has the following properties:
     - widgets_made_count - default 0
     - wear_and_tear_count - default 0
     - needs_reboot - default false
-
   A Machine has the following methods:
     - makeWidgets
         - This function takes in a number and increases widgets_made_count by that amount
@@ -99,10 +88,8 @@ class Employee {
     - fixMachine
         - This function sets needs_reboot to true
     - reboot
-        - This function returns a function that is called when the machine is done rebooting
-        - It should set decrease wear_and_tear_count by 10, and set needs_reboot to false
+        - This function returns an anonymous function that is called when the machine is done rebooting
+        - The anonymous function should decrease wear_and_tear_count by 10, and set needs_reboot to false
 */
 
 //Code Here
-
-
